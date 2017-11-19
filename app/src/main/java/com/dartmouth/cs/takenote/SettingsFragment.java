f298c8f1b338928946a96465081a2b2d1b4bf3a8
Acacia Hoisington <acacia.d.hoisington.18@dartmouth.edu>
2017-11-19 14:54:18 -0500

    settings fragment components make sense with app now.  we should add more to fill it in though.

--- app/src/main/java/com/dartmouth/cs/takenote/SettingsFragment.java
+++ app/src/main/java/com/dartmouth/cs/takenote/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.dartmouth.cs.takenote;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.EditTextPreference;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        //display user's email after input
+        EditTextPreference email = (EditTextPreference) findPreference("email");
+        email.setSummary("Please insert your email.");
+        email.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                preference.setSummary(newValue.toString());
+                return true;
+            }
+        });
     }
 }
 
